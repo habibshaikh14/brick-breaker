3ac226151ed113c7b1d706bb3edc2ee22843a3bc
Habib Shaikh <33100185+habibshaikh14@users.noreply.github.com>
2020-10-23 18:50:01 +0530

    A simple brick breaker game for learning puprose.

---
+++ BrickBreaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Configuratuin parameters
+    [SerializeField] float pushX = 2f;
+    [SerializeField] float pushY = 20f;
+    [SerializeField] float ballPositionOffsetY = 0.5f;
+    [SerializeField] GameObject paddle;
+    private bool ballInAir = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LaunchOnMouseClick();
+        ResetBallPosition();
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0) && !ballInAir)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(pushX, pushY);
+            ballInAir = true;
+        }
+    }
+
+    private void ResetBallPosition()
+    {
+        if(Input.GetMouseButtonDown(1) || !ballInAir)
+        {
+            Vector2 paddlePosition = paddle.transform.position;
+            Vector2 ballPosition = new Vector2(paddlePosition.x, paddlePosition.y + ballPositionOffsetY);
+            transform.position = ballPosition;
+            ballInAir = false;
+        }
+    }
+}
---
+++ BrickBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // Configuration Parameters
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    [SerializeField] float screenWidthInUnits = 16f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePositonXInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePosition = transform.position;
+        paddlePosition.x = Mathf.Clamp(mousePositonXInUnits, minX, maxX);
+        transform.position = paddlePosition;
+    }
+}
